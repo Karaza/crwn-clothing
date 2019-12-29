45115fea6da910b51e1bce9da197c04ae255a31c
Sophie Kowalski <skowalski57@gmail.com>
2019-12-29 11:34:17 +0100

    divide into smaller components and add styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <HomePage />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
